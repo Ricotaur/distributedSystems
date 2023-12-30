79b9ce121f67e97d06f8967539b6daf30d87854a
rihoit00 <rihoit00@hs-esslingen.de>
2023-12-30 05:15:26 +0100

    Frontend http calls working locally

--- todo-frontend/src/app/app.component.ts
+++ todo-frontend/src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Todo{
+  todo_id: number;
+  todo_task: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor( private http: HttpClient){}
+  
   title = 'todo-frontend';
-  todos : string[] = ['water plants','read the news','cook something new','work','work harder','','','','','','','','']
+  todos : Todo[] = [];
+  private URL = 'http://localhost:3000/';
+
+  ngOnInit(): void {
+    this.getTodos().subscribe(data =>{this.todos = data;});
+    console.log(this.todos);
+  }
+
+  getTodos(): Observable<Todo[]>{
+    return this.http.get<Todo[]>(this.URL + 'todos');
+  }
+  updateTodos(){
+    setTimeout(() => {
+      this.ngOnInit();
+    }, 100);
+  }
 }
