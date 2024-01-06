1d7a0f53a0032e484d1c3114ef4d092cf4d6d346
rihoit00 <rihoit00@hs-esslingen.de>
2024-01-06 18:54:36 +0100

    Docker fix working

--- todo-backend/src/app.module.ts
+++ todo-backend/src/app.module.ts
@@ -2,22 +2,16 @@ import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Todo } from './typeorm/entities/Todo';
 import { TodosModule } from './todos/todos.module';
 
 @Global()
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'host.docker.internal',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'todo_database',
-      entities: [Todo],
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
       synchronize: true,
-      logging: true,
     }),
     TodosModule,
   ],
