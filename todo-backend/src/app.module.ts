4c336d09f831c7d2bd41e7809caa170695ced566
rihoit00 <rihoit00@hs-esslingen.de>
2024-01-06 18:04:08 +0100

    Dockerized

--- todo-backend/src/app.module.ts
+++ todo-backend/src/app.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './typeorm/entities/Todo';
 import { TodosModule } from './todos/todos.module';
 
+@Global()
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'host.docker.internal',
       port: 3306,
       username: 'root',
       password: 'root',
